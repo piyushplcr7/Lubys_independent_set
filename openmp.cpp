42d8803f07974a2525cae4eaf4e59a6b11a555e6
root <root@jingqiu-Latitude-7480>
2017-11-02 12:08:45 +0100

    LinkedListVersionOpenMp

--- openmp.cpp
+++ openmp.cpp
@@ -3,34 +3,168 @@
 #include <omp.h> 
 #include<time.h>
 #include<cstdlib>
+#include<list>
+#include<iterator>
 #define rep(i,a,b,c) for(int i=a;i<b;i+=c)
-using std::vector;
-struct Graph {
+
+//typedef list<int>::iterator Vertex
+using namespace std;
+
+	class Edge;
+	class Vertex{
 	public:
-	int n_vertices;
+		/*
+		class Edge{
+		public:
+		list<Vertex>::iterator end;
+		list<Edge>::iterator same_edge;
+		Edge(list<Vertex>::iterator y)
+		{
+			end=y;
+		}
+		Edge()
+		{
+			end=NULL;
+			same_edge=NULL
+		}
+	};*/
+			list<Edge> edges;
+			int randval;
+			int id;
+			Vertex(int i)
+			{
+				id=i;
+				randval=0;
+			}
+
+			//~Vertex()
+			//{
+			//	delete [] edges;
+			//}
+		 int get_id()
+		 {
+		 	return id;
+		 }
+
+		/*Vertex(list<Vertex*>::iterator h)
+		{	
+			head=h;
+			//num_edges=0;
+		}
+		//void setlist(list<Vertex*>::iterator begin)
+		//{
+		//	head=begin;
+		//}*/
+	};
+	class Edge{
+		public:
+		list<Vertex>::iterator end;
+		list<Edge>::iterator same_edge;
+		Edge(list<Vertex>::iterator y)
+		{
+			end=y;
+		}
+	};
+
+class Graph {
+public:
+
+	//int n_vertices;
 	//Set of Vertices; contains 1 if vertex is in the graph and 0 if not in the graph
-	vector<int> vertices;
+	list<Vertex> vertices;
 	//std::array<int,n_vertices> vertices;
 	//set of Edges
 	//std::array<std:array<int,n_vertices>,n_vertices> edges;
-	vector<vector<int> > edges;
+	//list<list<Vertex*> > edges;
 	//constructor
-	Graph(int n):n_vertices(n),vertices(n,0),edges(n,vector<int>(n,0))
+	Graph(int n)
 	{
-			//create the matrix
-		rep(i,0,n,1)
-			vertices[i]=i;
+		rep(i,0,n,1){
+			vertices.push_back(Vertex(i));
+			//(vertices+i)->edges=list<Edge>();
+		}
+	//create the matrix
 	}
-	Graph(int n,double prob):n_vertices(n),vertices(n,0),edges(n,vector<int>(n,0))//random graph G(n_vertices,prob) where prob is the probability that an edge exists
+	Graph(int n,double prob)//random graph G(n_vertices,prob) where prob is the probability that an edge exists
 	{
-		rep(i,0,n,1)
-			vertices[i]=i;
+		//needs to be optimized
 		srand(time(NULL));
+		list<Vertex>::iterator source;
+		list<Vertex>::iterator dest;
 		rep(i,0,n,1)
+		vertices.push_back(Vertex(i));
+		for(source=vertices.begin();source!=vertices.end();++source)
 		{
-			rep(j,0,n,1)
-				edges[i][j]=(rand()*1.0/RAND_MAX<prob);
-			edges[i][i]=0;
+			for(dest=vertices.begin();dest!=source;++dest){
+				//edges[i][j]=(rand()*1.0/RAND_MAX<prob);
+				if(rand()*1.0/RAND_MAX<prob){
+					(source->edges).push_back(Edge(dest));
+					(dest->edges).push_back(Edge(source));
+					(source->edges).back().same_edge=--dest->edges.end();
+					(dest->edges).back().same_edge=--source->edges.end();
+					//cout<<"add edge"<<((source->edges).back().same_edge->end->id)<<"\t"<<(dest->edges).back().same_edge->end->id<<endl;					
+				}
+			}
+			source->randval=rand();
+		}
+	}
+	void showDegree()
+	{
+		for(list<Vertex>::iterator iter=vertices.begin();iter!=vertices.end();++iter)
+			cout<<"vertex"<<iter->id<<"degree"<<iter->edges.size()<<endl;		
+	}
+	void set_randval()
+	{
+		//srand(time(NULL));
+		for(list<Vertex>::iterator iter=vertices.begin();iter!=vertices.end();++iter)
+			iter->randval=rand();
+	}
+	bool isEmpty()
+	{
+		return vertices.empty();
+	}
+	void choose(vector<list<Vertex>::iterator> &chosen_set)
+	{
+		for(list<Vertex>::iterator iter=vertices.begin();iter!=vertices.end();++iter)
+		{
+		//	cout<<"1/d:"<<1.0/(2*iter->edges.size())<<"randval"<<iter->randval<<endl;
+			if(iter->randval>0 && (iter->randval*1.0/RAND_MAX)<(1.0/(2*iter->edges.size())))
+			{
+				for(list<Edge>::iterator eiter=iter->edges.begin();eiter!=iter->edges.end();++eiter)
+					eiter->end->randval=-1;
+				chosen_set.push_back(iter);
+			}
+		}
+	}
+	void deleteNodeNeighbor(list<Vertex>::iterator pvertex)
+	{
+		cout<<"deleteNodeneighbor"<<pvertex->id<<endl;
+		//for(list<Edge>::iterator neighbor_edge=(pvertex->edges).begin();neighbor_edge!=(pvertex->edges).end();neighbor_edge++)
+		//	cout<<neighbor_edge->end->id<<endl;
+		for(list<Edge>::iterator neighbor_edge=(pvertex->edges).begin();neighbor_edge!=(pvertex->edges).end();neighbor_edge++)
+			deleteNode(neighbor_edge->end,pvertex);
+		vertices.erase(pvertex);
+	}
+	void deleteNode(list<Vertex>::iterator pvertex,list<Vertex>::iterator not_delete)
+	{
+		cout<<"delete Node"<<pvertex->id<<endl;
+
+		for(list<Edge>::iterator neighbor_edge=(pvertex->edges).begin();neighbor_edge!=(pvertex->edges).end();neighbor_edge++)
+		{
+			if(neighbor_edge->end!=not_delete){
+			neighbor_edge->end->edges.erase(neighbor_edge->same_edge);
+			//cout<<neighbor_edge->end->id<<"\t"<<neighbor_edge->end->edges.size()<<endl;
+		}
+		}
+		//cout<<"delete end"<<endl;
+		vertices.erase(pvertex);
+	}
+	void deleteSet(vector<list<Vertex>::iterator>::iterator begin,vector<list<Vertex>::iterator>::iterator end)
+	{
+		for(vector<list<Vertex>::iterator>::iterator viter=begin;viter!=end;++viter)
+		{
+			deleteNodeNeighbor(*viter);
+			//cout<<(*viter)->id<<endl;
 		}
 	}
 		//create the matrix
@@ -41,60 +175,45 @@ struct Graph {
 			}
 		};*/
 	//destructor
-	/*~Graph(){
-		delete [] vertices;
-		for (int i = 0; i < n_vertices; ++i)
-    		delete [] edges[i];
-		delete [] edges;
-		};*/
+	//~Graph(){
+	//	delete [] vertices;
+	//	};
 };
 
 int main(int argc, char** argv)
 {
-       //Test Graph
-	/*Graph G(8);
-	for(int i=0;i<8;i++)
-              for(int j=0;j<8;j++)
-                     G.edges[i][j]=0;
-
-	G.edges[1][4]=1; G.edges[4][1]=1;
-	G.edges[1][2]=1; G.edges[2][1]=1;
-	G.edges[1][0]=1; G.edges[0][1]=1;
-	G.edges[4][5]=1; G.edges[5][4]=1;
-	G.edges[4][7]=1; G.edges[7][4]=1;
-	G.edges[0][7]=1; G.edges[7][0]=1;
-	G.edges[5][2]=1; G.edges[2][5]=1;
-	G.edges[3][0]=1; G.edges[0][3]=1;
-	G.edges[6][3]=1; G.edges[3][6]=1;
-	G.edges[3][2]=1; G.edges[2][3]=1;
-	G.edges[5][6]=1; G.edges[6][5]=1;
-	G.edges[6][7]=1; G.edges[7][6]=1;*/
 	int n_vertices=100;
 	double prob=0.5;
 	Graph G(n_vertices,prob);
-	vector<int> dv(n_vertices,0);
+	G.showDegree();
+	vector<list<Vertex>::iterator > chosen_set;
+	vector<int> independent_set;
     double time;
-    
-    time = -omp_get_wtime();
+    cout<<"contruction end"<<endl;
+    int i=0;
+    //time = -omp_get_wtime();
 
-	#pragma omp parallel for
-	       rep(i,0,n_vertices,1)
-              {
-                     rep(j,0,n_vertices,1)
-                            dv[i] = dv[i]+(G.edges[i][j]);
-                     int this_thread = omp_get_thread_num();
-                     int num_threads = omp_get_num_threads();
-                     std::cout<<"thread index is:"<<this_thread<<std::endl;
-              }
-
-
-
-	rep(i,0,n_vertices,1)
-		std::cout<<"dv for index "<<i<<"="<<dv[i]<<std::endl;
-
-	time += omp_get_wtime();
-
+	//#pragma omp parallel for
+			while(!G.isEmpty()){
+				cout<<"round"<<++i<<endl;
+				G.set_randval();
+				cout<<"Rand val generated"<<endl;
+				G.choose(chosen_set);
+				cout<<"The independent set nodes chosen"<<endl;
+				//rep(i,0,chosen_set.size(),1)
+				//	cout<<chosen_set[i]->id<<endl;
+				for(vector<list<Vertex>::iterator >::iterator iter=chosen_set.begin();iter!=chosen_set.end();++iter)
+					independent_set.push_back((*iter)->id);
+				G.deleteSet(chosen_set.begin(),chosen_set.end());
+				cout<<" Nodes and neighbors deleted"<<endl;
+				chosen_set.clear();
+				cout<<" Vertex size"<<G.vertices.size()<<endl;
+          	}
+    cout<<"independent set found"<<endl;
+	rep(i,0,independent_set.size(),1)
+		std::cout<<"node"<<i<<"in MIS:\t"<<independent_set[i]<<std::endl;
+	cout<<chosen_set.size()<<endl;
+	//time += omp_get_wtime();
 	std::cout<<"it took time:"<<time<<std::endl; 
-
-       return 0;
+    return 0;
 }
